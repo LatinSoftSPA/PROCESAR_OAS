81e3606fb5588f24551a18a79d443c0250f03ea7
LatinSoft SPA <latinsoft.chile@gmail.com>
2023-03-28 13:56:51 -0300

    Algoritmo Terminado Con Exito...!!!

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import CSVToJSON from "csvtojson";
+import { appendFile  } from 'fs/promises';
 
 const LISTADO_OAS = "archivos/OA.csv";
 
@@ -8,59 +9,46 @@ const dataFromCSV = async (path) => {
 };
 
 const filtrarOAS = (listado, nivel, curso, asignatura) => {
-  let RESPUESTA = [
-    // {
-    // asignatura: {
-    // matematicas: {
-    //   codigo_asignatura: null,
-    //   categorias: {
-    //     actitudes: [],
-    //     basales: [],
-    //     complementarios: [],
-    //     transversales: [],
-    //     habilidades: [],
-    //   },
-    // },
-    // },
-    // },
-
-  ];
-  const listadoFiltrado = listado.filter(
-    (obj) => obj.NIVEL === nivel && obj.CURSO === curso && obj.ASIGNATURA === asignatura
-  );
-
+  let RESPUESTA = [];
+  const listadoFiltrado = listado.filter((obj) => obj.NIVEL === nivel && obj.CURSO === curso && obj.ASIGNATURA === asignatura);
   const aux = {};
-  const actitudes = [];
-  const basales = [];
-  const complementarios = [];
-  const transversales = [];
-  const habilidades = [];
+  const categorias = {};
   aux.curso = curso;
   aux.nivel = nivel;
-  listadoFiltrado.forEach((obj) => {
-    const { NIVEL, CURSO, OA, COD_ASG, ASIGNATURA, CATEGORIA, DESCRIPCION } = obj;
-    const categorias = {
-      actitudes: actitudes,
-      basales: basales,
-      complementarios: complementarios,
-      transversales: transversales,
-      habilidades: habilidades,
-    };
-    categorias[CATEGORIA] = [];
-    aux[ASIGNATURA] = {
-      codigo_asignatura: COD_ASG,
+  
+  for(let key in listadoFiltrado){
+    const actual = listadoFiltrado[key];
+    if(!categorias[actual.CATEGORIA]) categorias[actual.CATEGORIA] = [];
+    else categorias[actual.CATEGORIA].push({
+      codigo_oa: actual.OA,
+      descripcion: actual.DESCRIPCION
+    });
+
+    aux[actual.ASIGNATURA] = {
+      codigo_asignatura: actual.COD_ASG,
       categorias: categorias,
     };
-  });
+  }
   RESPUESTA.push(aux);
-
-  console.log(RESPUESTA[0]["Matemática"]["categorias"]);
-  // Object.keys(object1);
+  return RESPUESTA;
 };
+//*************************************************************************************  */
+
+const appendToFile = async (fileName, data) => {
+  try {
+    await appendFile(fileName, data, { flag: 'w' });
+    console.log(`Appended data to ${fileName}`);
+  } catch (error) {
+    console.error(`Got an error trying to append the file: {error.message}`);
+  }
+}
+//*************************************************************************************  */
 
 const listadoJSON = await dataFromCSV(LISTADO_OAS);
 
 const nivel = "B";
 const curso = "1B";
 const asignatura = "Matemática";
-filtrarOAS(listadoJSON, nivel, curso, asignatura);
+const respuesta =  filtrarOAS( listadoJSON, nivel, curso, asignatura);
+
+await appendToFile('salida.json', JSON.stringify(respuesta));
